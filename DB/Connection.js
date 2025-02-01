8dec3ec9c082162b992df3dc3f16ec199acc2bb3
Biswarup Ghosh <ghoshbiswarup414@gmail.com>
2025-02-02 02:41:43 +0530

    updated the connection file

--- DB/Connection.js
+++ DB/Connection.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const ConnectToDB = (url) => {
     if (!url) return;
     try {
-        mongoose.connect(url)
-            .then(() => {
-                console.log("Connected to DB");
+        mongoose
+            .connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
             })
+            .then(() => console.log("MongoDB Connected"))
+            .catch((err) => console.error("MongoDB Connection Error:", err));
     } catch (error) {
         console.log(`Error occurred: ${err}`);
     }
